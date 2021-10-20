480aed113205ad336892dec60579b96c8827ae62
Francisca105 <65908870+Francisca105@users.noreply.github.com>
2021-10-20 12:17:13 +0100

    Delete handlers directory

--- handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-    let prompt = process.openStdin()
-    prompt.addListener("data", res => {
-        let x = res.toString().trim().split(/ +/g)
-            bot.channels.get("677962680933285894").send(x.join(" "));
-        });
-    }
